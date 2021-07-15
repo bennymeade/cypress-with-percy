781e261d07244ad2aca41a69b792b46e7ad839f4
bennymeade <brendan_meade@yahoo.co.uk>
2021-07-15 21:13:12 +0100

    initial project checkin

---
+++ cypress/integration/visual.spec.js
@@ -0,0 +1,10 @@
+const urls = ['https://www.unilever.com.my/news/press-releases/', 'https://www.unilever.com.my/news/', 'https://www.unilever.com.my/', 'https://www.unilever.com.my/news/2021/how-we-will-help-build-a-more-equitable-and-inclusive-society/'];
+
+describe('visual', () => {
+    urls.forEach(url => {
+    it(`connect to percy - ${url}`, () => {
+        cy.visit(url)
+        cy.percySnapshot(url, { enableJavaScript: true });
+    })
+    })
+})
